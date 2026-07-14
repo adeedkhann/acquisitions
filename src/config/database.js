731c8ab83ddf164d60bf61340af3e9a4482c662a
adeedkhann <adeedkhan0786@gmail.com>
2026-07-14 18:47:09 +0530

    setup prisma and migrate user model

---
+++ src/config/database.js
@@ -0,0 +1,21 @@
+import 'dotenv.config';
+import { PrismaClient } from '@prisma/client';
+
+
+const prisma = new PrismaClient({
+  log: process.env.NODE_ENV === 'development' ? ['query' , 'info' , 'warn' , 'error'] : ['error'],
+});
+
+
+export const connctDB = async()=>{
+  try {
+    await prisma.$connect();
+    console.log('neon db connect success');
+  } catch (error) {
+    console.log('db connect failed ' , error.message);
+    process.exit(1);
+  }
+};
+
+
+export default prisma;
